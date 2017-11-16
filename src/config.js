08deb62bc90f7d858e5525ce84c3fcbbef40a28d
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-11-16 17:27:13 +0100

    fix: tests ()

--- src/config.js
+++ src/config.js
@@ -9,24 +9,21 @@ let $b = require("bluebird");
 let debugConfig = require("debug")("grmcheck_config");
 
 function readConfig(config) {
-  debugConfig("Original config");
-  debugConfig(config);
   let data;
   if (!_.isUndefined(config.file) && _.isUndefined(config.rulefile)) {
     let rulefile = _.get(config, "rulefile", "/.gramchk.yml");
-    debugConfig(`Using rulefile ${rulefile}`);
     data = read(rulefile, { cwd: path.dirname(config.file) });
-    debugConfig(`Read-data ${data}`);
     data = $yaml.safeLoad(data);
-    debugConfig(`Interpreted-data ${data}`);
     return $b.resolve(_.assign(config, data));
   } else {
-    return readFile(config.rulefile).then(data => {
-      debugConfig(`Read-data ${data}`);
-      data = $yaml.safeLoad(data);
-      debugConfig(`Interpreted-data ${data}`);
-      return $b.resolve(_.assign(config, data));
-    });
+    if (!_.isUndefined(config.rulefile)) {
+      return readFile(config.rulefile).then(data => {
+        data = $yaml.safeLoad(data);
+        return $b.resolve(_.assign(config, data));
+      });
+    } else {
+      return $b.resolve(config);
+    }
   }
 }
 
